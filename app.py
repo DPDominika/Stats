0ebd9bc3cb7314773755a614d9b8024e287e488e
DPDominika <dominika.plesniak@gmail.com>
2018-04-26 22:44:05 +0200

    added dropdown component and functions in helpers module

--- app.py
+++ app.py
@@ -5,12 +5,11 @@ import helpers as helper
 
 app = dash.Dash()
 
-id_list = helper.prepare_resources()
-all_options = helper.create_all_options(id_list)
-
-vars_data = helper.create_data(all_options, id_list, 'VARS')
-cvrg_data = helper.create_data(all_options, id_list, 'CVRG')
-
+id_list = helper.prepare_resources('stats/single_run_stats/')
+options = helper.create_all_options(id_list)
+vars_data = helper.create_data(options, id_list, 'VARS')
+cvrg_data = helper.create_data(options, id_list, 'CVRG')
+# yield_data = {'ALL_RUNS': {'x': helper.extract('stats/all_run_stats.tsv', 'RUN_ID'), 'y': helper.extract('stats/all_run_stats.tsv', 'YIELD') }}
 
 app.css.append_css({'external_url': 'https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css'})
 
@@ -30,10 +29,9 @@ app.layout = html.Div(
                         html.P('Choose runs:'),
                         dcc.Dropdown(
                                 id ='Runs',
-                                options=[{'label': k, 'value': k} for k in all_options],
-                                value=all_options,
-                                multi=True
-
+                                options=[{'label': k, 'value': k} for k in options],
+                                value='RUN_ID1',
+                                clearable=False
                         ),
                     ],
                     className='six columns',
@@ -64,18 +62,13 @@ app.layout = html.Div(
 
 
 @app.callback(
-    dash.dependencies.Output('Runs', 'options'),
-    [dash.dependencies.Input('Runs', 'value')])
-
-
-@app.callback(
     dash.dependencies.Output('graph', 'figure'),
     [dash.dependencies.Input('Runs', 'value')])
 def update_image_src(selector):
     data = []
-    for cvrg in selector:
-        data.append({'x': cvrg_data[cvrg]['x'], 'y': cvrg_data[cvrg]['y'],
-                    'type': 'bar', 'name': cvrg})
+    if selector:
+        data.append({'x': cvrg_data[selector]['x'], 'y': cvrg_data[selector]['y'], 'type': 'bar', 'name': selector})
+
     figure = {
         'data': data,
         'layout': {
@@ -104,9 +97,9 @@ def update_image_src(selector):
     [dash.dependencies.Input('Runs', 'value')])
 def update_image_src(selector):
     data = []
-    for vars in selector:
-        data.append({'x': vars_data[vars]['x'], 'y': vars_data[vars]['y'],
-                    'type': 'bar', 'name': vars})
+    if selector:
+        data.append({'x': vars_data[selector]['x'], 'y': vars_data[selector]['y'], 'type': 'bar', 'name': selector})
+
     figure = {
         'data': data,
         'layout': {
--- helpers.py
+++ helpers.py
@@ -2,8 +2,7 @@ import csv
 import os
 
 
-def prepare_resources():
-    directory = 'stats/single_run_stats/'
+def prepare_resources(directory):
     id_list = []
     for file in os.listdir(directory):
         if os.path.splitext(file)[1] == '.tsv':
@@ -12,10 +11,10 @@ def prepare_resources():
 
 
 def create_all_options(id_list):
-    all_options = []
+    options = []
     for i in range(1, len(id_list) + 1):
-        all_options.append('RUN_ID' + str(i))
-    return all_options
+        options.append('RUN_ID' + str(i))
+    return options
 
 
 def extract(file, data_name):
@@ -27,17 +26,12 @@ def extract(file, data_name):
         return data
 
 
-def create_data(all_options, id_list, data_name):
-    vars_data = {}
-    for i in range(len(all_options)):
-        vars_data[all_options[i]] = {'x': extract('stats/single_run_stats/' + id_list[i], 'SAMPLE'), 'y': extract('stats/single_run_stats/' + id_list[i], data_name)}
-    return vars_data
+def create_data(options, id_list, data_name):
+    data = {}
+    for i in range(len(options)):
+        data[options[i]] = {'x': extract('stats/single_run_stats/' + id_list[i], 'SAMPLE'), 'y': extract('stats/single_run_stats/' + id_list[i], data_name)}
+    return data
 
 
 
 
-# print(extract('stats/single_run_stats/180317.tsv', 'SAMPLE'))
-# id_list = prepare_resources()
-# print(id_list)
-# all_options = create_all_options(id_list)
-# print(create_vars_data(all_options, id_list))
